d4bc58c88808af52915a845e7bc9b4112b923043
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-03-21 08:05:35 -0600

    change methhod

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify,request
 from flask_cors import CORS, cross_origin
 from Arbol import Nodo
 from DFS_rec import buscar_solucion_DFS_rec
-from BFS import buscar_solucion_BFS
+from puzzle import buscar_solucion_BFS
 from DFS import buscar_solucion_DFS
 
 app = Flask(__name__)
@@ -33,28 +33,17 @@ def solucion():
 @app.route("/bfs", methods = ["POST"])
 @cross_origin()
 def solucion_bfs():
-    conexiones = {
-        'CDMX': {'SLP','MEXICALI', 'CHIHUAHUA'},
-        'SAPOPAN': {'ZACATECAS', 'MEXICALI'},
-        'GUADALAJARA':{'CHIAPAS'},
-        'CHIAPAS':{'CHIHUAHUA'},
-        'MEXICALI':{'SLP', 'SAPOPAN', 'CDMX', 'CHIHUAHUA', 'SONORA'},
-        'SLP':{'CDMX', 'MEXICALI'},
-        'ZACATECAS':{'SAPOPAN', 'SONORA', 'CHIHUAHUA'},
-        'SONORA':{'ZACATECAS', 'MEXICALI'},
-        'MICHOACAN':{'CHIHUAHUA'},
-        'CHIHUAHUA':{'MICHOACAN', 'ZACATECAS', 'MEXICALI', 'CDMX','CHIAPAS' }
-    }
     body = request.json
     estado_inicial = body['estadoInicial']
-    solucion = body['destino']
-    nodo_solucion = buscar_solucion_BFS(conexiones, estado_inicial, solucion)
-    # Mostrar Resultado
+    solucion = body['solucion']
+    nodo_solucion = buscar_solucion_BFS(estado_inicial, solucion)
+    # Mostrar resultado 
     resultado = []
     nodo = nodo_solucion
-    while nodo.get_padre() != None:
+    while nodo.get_padre() is not None:
         resultado.append(nodo.get_datos())
         nodo = nodo.get_padre()
+
     resultado.append(estado_inicial)
     resultado.reverse()
     return str(resultado)

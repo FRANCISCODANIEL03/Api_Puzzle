9564782cac257499c290eba14a1403a1b1dc9a2c
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-03-31 22:04:53 -0600

    add function to calculate the distance

--- VRP.py
+++ VRP.py
@@ -1,3 +1,10 @@
 #VRP con restricciones adicionales
 import math 
 from operator import itemgetter
+
+# ------------------------- Funciones auxiliares -------------------------
+
+def distancia(coord1, coord2):
+    lat1, long1 = coord1
+    lat2, long2 = coord2
+    return math.sqrt((lat1 - lat2)**2 + (long1 - long2)**2)

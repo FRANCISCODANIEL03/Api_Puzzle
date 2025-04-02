95998698f7aaafa9e13a2fc4feafdd3b9ec5cd2d
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-04-01 22:08:00 -0600

    calculate savings

--- VRP.py
+++ VRP.py
@@ -23,3 +23,19 @@ def clientes_en_ruta(ruta):
 
 def esta_restringido(ciudad1, ciudad2, restricciones):
     return (ciudad1, ciudad2) in restricciones or (ciudad2, ciudad1) in restricciones
+
+# --------------------------- Algoritmo VRP ------------------------------
+
+def vrp_voraz(coord, pedidos, almacen, max_carga, max_clientes, restricciones_trafico):
+    # Calcular los ahorros 
+    s = {}
+    for c1 in coord:
+        for c2 in coord:
+            if c1 != c2 and not (c2, c1) in s:
+                if not esta_restringido(c1, c2, restricciones_trafico):
+                    d_c1_c2 = distancia(coord[c1], coord[c2])
+                    d_c1_almacen = distancia(coord[c1], almacen)
+                    d_c2_almacen = distancia(coord[c2], almacen)
+                    s[c1, c2] = d_c1_almacen + d_c2_almacen - d_c1_c2
+
+    s = sorted(s.items(), key=itemgetter(1), reverse=True)

ade615bd5b75e27459d5be26adcac5cdf483f9f4
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-04-01 22:05:25 -0600

    add new function

--- VRP.py
+++ VRP.py
@@ -8,3 +8,9 @@ def distancia(coord1, coord2):
     lat1, long1 = coord1
     lat2, long2 = coord2
     return math.sqrt((lat1 - lat2)**2 + (long1 - long2)**2)
+
+def en_ruta(rutas, ciudad):
+    for r in rutas:
+        if ciudad in r:
+            return r
+    return None

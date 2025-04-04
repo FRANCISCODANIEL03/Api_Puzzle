c8f6db90f7393b7d99383deb9d7314b0e6df3cbd
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-04-03 22:13:20 -0600

    add new function

--- api-hill/HILL_C.py
+++ api-hill/HILL_C.py
@@ -1,3 +1,10 @@
 # TSP con Hill Climning
 import math
 import random
+
+def distancia(coord1, coord2):
+    lat1 = coord1[0]
+    lon1 = coord1[1]
+    lat2 = coord2[0]
+    lon2 = coord2[1]
+    return math.sqrt((lat1 -lat2) **2 + (lon1 - lon2)**2)

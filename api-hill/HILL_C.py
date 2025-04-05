36d7131ae23c3ec41c0cb359a30fd98d9c64bab6
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-04-04 22:13:48 -0600

    calculate the best route

--- api-hill/HILL_C.py
+++ api-hill/HILL_C.py
@@ -20,3 +20,32 @@ def evalua_ruta(ruta, coord):
     ciudad2 = ruta[0]
     total = total + distancia(coord[ciudad1], coord[ciudad2])
     return total
+
+def hill_climbing(coord):
+    # Crear la ruta inicial Aleatoria
+    ruta = []
+    for ciudad in coord:
+        ruta.append(ciudad)
+    random.shuffle(ruta)
+    
+    mejora = True
+    while mejora:
+        mejora = False
+        dist_actual = evalua_ruta(ruta, coord)
+        # Evaluar Vecinos
+        for i in range (0, len(ruta)):
+            if mejora:
+                break
+            for j in range(0, len(ruta)):
+                if i!= j:
+                    ruta_tmp = ruta[:]
+                    ciudad_tmp = ruta[i]
+                    ruta_tmp[i] = ruta_tmp[j]
+                    ruta_tmp[j] = ciudad_tmp
+                    dist = evalua_ruta(ruta_tmp, coord)
+                    if dist < dist_actual:
+                        # Se ha encontrado un vecino que mejora el resultado
+                        mejora = True
+                        ruta = ruta_tmp[:]
+                        break
+    return ruta

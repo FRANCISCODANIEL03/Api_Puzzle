c870b028b86431eba3ba1e4a884f3a93dadaa51b
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-04-06 22:44:40 -0600

    change files

--- dijkstra.py
+++ dijkstra.py
@@ -2,18 +2,14 @@ from Arbol import Nodo
 import networkx as nx
 import matplotlib.pyplot as plt
 import mysql.connector
-import os
-from dotenv import load_dotenv
-# Cargar variables de entorno
-load_dotenv()
 
 def conectar_base_datos():
     # Conexión a la base de datos
     conexion = mysql.connector.connect(
-        host=os.getenv("MYSQL_HOST"),
-        user=os.getenv("MYSQL_USER"),
-        password=os.getenv("MYSQL_PASSWORD"),
-        database=os.getenv("MYSQL_DATABASE")
+        host="bltpsedcjatmi5mobxi6-mysql.services.clever-cloud.com",
+        user="uzfprw81c47ssrq8",
+        password="IbRqoqg9GZU4ln01Z0V4",
+        database="bltpsedcjatmi5mobxi6"
     )
 
     cursor = conexion.cursor()

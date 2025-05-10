d57422c4236b979a30fa2a7241de4f6a0ffcaf63
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-05-09 19:07:07 -0600

    add route evaluation function to calculate total distance

--- TSP-TABU/tabu-mapa.py
+++ TSP-TABU/tabu-mapa.py
@@ -7,3 +7,9 @@ def distancia(coord1, coord2):
     lat1, long1 = coord1
     lat2, long2 = coord2
     return math.sqrt((lat1 - lat2) ** 2 + (long1 - long2) ** 2)
+
+def evalua_ruta(ruta, coord):
+    total = 0
+    for i in range(len(ruta) - 1):
+        total += distancia(coord[ruta[i]], coord[ruta[i + 1]])
+    return total 

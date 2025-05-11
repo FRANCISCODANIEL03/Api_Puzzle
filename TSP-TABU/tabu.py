8b2fc626efa0ebf2d42674f3b0672df8976f2f8e
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-05-10 19:11:06 -0600

    implement evalua_ruta function to evaluate the total distance of a route

--- TSP-TABU/tabu.py
+++ TSP-TABU/tabu.py
@@ -8,3 +8,12 @@ def distancia(coord1, coord2):
     lat2 = coord2[0]
     long2 = coord2[1]
     return math.sqrt((lat1 - lat2) ** 2 + (long1 - long2) ** 2)
+
+def evalua_ruta(ruta, coord):
+    total = 0
+    for i in range(len(ruta) - 1):
+        ciudad1 = ruta[i]
+        ciudad2 = ruta[i + 1]
+        total += distancia(coord[ciudad1], coord[ciudad2])
+    total += distancia(coord[ruta[-1]], coord[ruta[0]])  # volver al inicio
+    return total

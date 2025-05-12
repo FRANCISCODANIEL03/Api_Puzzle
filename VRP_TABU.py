b8f5af3ea9c62b6138ae768b6e9edc94159bca29
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-05-11 19:15:43 -0600

    add distancia_total function to calculate total distance of routes

--- VRP_TABU.py
+++ VRP_TABU.py
@@ -18,3 +18,13 @@ def clientes_en_ruta(ruta):
 
 def esta_restringido(ciudad1, ciudad2, restricciones):
     return (ciudad1, ciudad2) in restricciones or (ciudad2, ciudad1) in restricciones
+
+def distancia_total(rutas, coord, almacen):
+    total = 0
+    for ruta in rutas:
+        if ruta:
+            total += distancia(almacen, coord[ruta[0]])  # del almacén al primero
+            for i in range(len(ruta) - 1):
+                total += distancia(coord[ruta[i]], coord[ruta[i+1]])
+            total += distancia(coord[ruta[-1]], almacen)  # del último al almacén
+    return total

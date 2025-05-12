844b06aff26c96cabc149ecf63271b487948256b
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-05-11 19:13:45 -0600

    add auxiliary functions for distance calculation

---
+++ VRP_TABU.py
@@ -0,0 +1,11 @@
+import math
+import random
+from operator import itemgetter
+from copy import deepcopy
+
+# ------------------------- Funciones auxiliares -------------------------
+
+def distancia(coord1, coord2):
+    lat1, long1 = coord1
+    lat2, long2 = coord2
+    return math.sqrt((lat1 - lat2)**2 + (long1 - long2)**2)

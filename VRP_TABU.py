93fdf8016b4de55449ccd6838371351732356d9c
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-05-11 19:14:10 -0600

    add peso_ruta function to calculate total weight of a route

--- VRP_TABU.py
+++ VRP_TABU.py
@@ -9,3 +9,6 @@ def distancia(coord1, coord2):
     lat1, long1 = coord1
     lat2, long2 = coord2
     return math.sqrt((lat1 - lat2)**2 + (long1 - long2)**2)
+
+def peso_ruta(ruta, pedidos):
+    return sum(pedidos[c] for c in ruta)

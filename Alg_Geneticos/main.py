9e2aef3efdccfc1ed1043614376f82a3807abe70
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-05-14 20:03:47 -0600

    add initial implementation of poblacion_inicial function to create random population

---
+++ Alg_Geneticos/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import math
+import random
+
+def poblacion_inicial(max_poblacion, num_vars):
+    # Crear población inicial aleatoria
+    poblacion = []
+    for i in range(max_poblacion):
+        gen=[]
+        for j in range(num_vars):
+            if random.random() > 0.5:
+                gen.append(1)
+            else:
+                gen.append(0)
+        poblacion.append(gen[:])
+    return poblacion

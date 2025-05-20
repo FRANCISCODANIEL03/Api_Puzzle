034cb18e7e8b15fe643d198da11d3282c21f4318
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-05-19 20:22:40 -0600

    add fetch request to send datos object on btn-generar click

--- Alg_Geneticos/frontend/script.js
+++ Alg_Geneticos/frontend/script.js
@@ -7,4 +7,14 @@ document.getElementById('btn-generar').addEventListener('click', async () => {
 	"prob_mutacion": 0.1
   };
 
+  try {
+    const respuesta = await fetch('http://172.168.3.215:5001/genes', { // cambia la URL
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(datos)
+    });
+
+  }
 });

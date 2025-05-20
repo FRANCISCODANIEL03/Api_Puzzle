de84c3a79da473b7242b43214c80f9e0d46b3413
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-05-19 20:22:10 -0600

    add event listener for btn-generar and initialize datos object

---
+++ Alg_Geneticos/frontend/script.js
@@ -0,0 +1,10 @@
+document.getElementById('btn-generar').addEventListener('click', async () => {
+
+  const datos = {
+    "max_iter":10,
+	"max_poblacion":50,
+	"num_vars":10,
+	"prob_mutacion": 0.1
+  };
+
+});

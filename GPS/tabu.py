67f9520cfbbace71dce81054796511cd8db15624
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2025-06-15 19:02:48 -0600

    add route evaluation function to calculate total distance

--- GPS/tabu.py
+++ GPS/tabu.py
@@ -10,3 +10,10 @@ def distancia(coord1, coord2):
     lat1, lon1 = coord1
     lat2, lon2 = coord2
     return abs(lat1 - lat2) + abs(lon1 - lon2)
+
+# Evaluación de la ruta total
+def evalua_ruta(ruta, coord):
+    total = 0
+    for i in range(len(ruta) - 1):
+        total += distancia(coord[ruta[i]], coord[ruta[i + 1]])
+    return total
